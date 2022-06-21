2088e8c53a3787a6014224af68afdfb9bc13545e
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-06-21 22:30:48 +0200

    Added the First Working Spider of ASINs Extractor

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# ASINs_Extractor
+# Movies Quotes
+
+This actor allows you to collect quotes from movies by a keyword of your choice.
+ 
+## Input configuration
+
+The actor has the following input options:
+
+- **Keyword** - type the keyword(s) you would like to a quotes for.
+- **Proxy** - Optionally, select a proxy to be used by the actor,
+  in order to avoid IP address-based blocking by the target website.
+  The actor automatically executes all the Scrapy's HTTP(S) requests through the proxy.
+
+## Results:
+
+Each record represent a title. The following fields is the current data you will get from the scraper...
+
+```json
+{
+    "id": "tt4574334",
+    "title": "Stranger Things",
+    "quotes": ["Friends don't lie"],
+    "year": "2016–",
+    "runtime": "51 min",
+    "genre": "Drama, Fantasy, Horror",
+    "rating": "8.7",
+    "url": "https://www.imdb.com/title/tt4574334/",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BODZlYjQ4NzYtZTg1MC00NGY4LTg4NjQtNGE3ZjRkMjk3YjMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY98_CR5,0,67,98_AL_.jpg",
+    "big_poster_url": "https://m.media-amazon.com/images/M/MV5BODZlYjQ4NzYtZTg1MC00NGY4LTg4NjQtNGE3ZjRkMjk3YjMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+  }
+```
+
+If you have any problem or anything does not work,
+please file an [issue on Apify](https://console.apify.com/actors/ZZQ9zLu3lwula6mup#/issues).
---
+++ actor/spiders/run.py
@@ -0,0 +1,79 @@
+import re
+import os
+import apify
+import logging
+from scrapy import Spider
+from urllib.parse import urljoin
+from apify_client import ApifyClient
+from scrapy.http.request import Request
+
+
+class ASINsExtractor(Spider):
+
+    name = 'asins_extractor'
+
+    headers = {'Host': 'www.amazon.com',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Language': 'en-GB,en;q=0.5',
+               'Accept-Encoding': 'gzip, deflate',
+               'Connection': 'keep-alive',
+               'Upgrade-Insecure-Requests': '1',}
+
+    logger = None
+
+    first_page_only = False
+    total_number_of_inputs_urls = 0
+    inputs_urls = ['https://www.amazon.com/s?k=python']
+
+    directory_path = os.getcwd()
+
+    def start_requests(self):
+
+        self.logger = logging.getLogger()
+
+        if 'Dropbox' not in self.directory_path:
+
+            # Initialize the main ApifyClient instance
+            client = ApifyClient(os.environ['APIFY_TOKEN'], api_url=os.environ['APIFY_API_BASE_URL'])
+
+            # Get the resource subclient for working with the default key-value store of the actor
+            default_kv_store_client = client.key_value_store(os.environ['APIFY_DEFAULT_KEY_VALUE_STORE_ID'])
+
+            # Get the value of the actor input and print it
+            self.logger.info('Loading input...')
+            actor_input = default_kv_store_client.get_record(os.environ['APIFY_INPUT_KEY'])['value']
+            self.logger.info(actor_input)
+
+            self.inputs_urls = actor_input["inputs_urls"]
+            self.first_page_only = actor_input["first_page_only"]
+
+        for input_url in self.inputs_urls:
+            yield Request(url=input_url,
+                          headers=self.headers,
+                          callback=self.parse_overview_page)
+
+    def parse_overview_page(self, response):
+
+        results = response.xpath('.//div/@data-asin')
+        if results and len(results) > 0:
+            results = results.extract()
+            for result in results:
+                if result != '':
+                    obj = {'ASIN': result,
+                           'Overview Page URL': response.url}
+
+                    if 'Dropbox' not in self.directory_path:
+                        apify.pushData(obj)
+                    else:
+                        yield apify
+
+        if not self.first_page_only:
+            next_url = response.xpath('.//a[contains(@class, "s-pagination-item s-pagination-next")]/@href')
+            if next_url and len(next_url) > 0:
+                next_url = next_url.extract()[0]
+                next_url = urljoin(self.base_url, next_url)
+                yield Request(url=next_url,
+                              meta=response.meta,
+                              headers=self.headers,
+                              callback=self.parse_overview_page)
