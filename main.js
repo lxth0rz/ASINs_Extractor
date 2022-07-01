611b91acbb4556dd1bb93f9cbf6c34b6e9429df6
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-07-01 14:42:33 +0200

    enable input_url

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ Apify.main(async () => {
     // Apify.openRequestQueue() creates a preconfigured RequestQueue instance.
     // We add our first request to it - the initial page the crawler will visit.
 
-    //const { startUrls } = await Apify.getInput();
-    const startUrls = [
-        'https://www.amazon.com/s?k=python&crid'
-    ];
+    const { startUrls } = await Apify.getInput();
+    // const startUrls = [
+    //     'https://www.amazon.com/s?k=python&crid'
+    // ];
 
     const requestList = await Apify.openRequestList('start-urls', startUrls);
     const requestQueue = await Apify.openRequestQueue();
